26c0c57aba85a64b86ed1da7d73391d5f6e87cd0
Sofia Bala <114072833+SophiaBala@users.noreply.github.com>
2024-09-16 18:31:42 +0300

    Update request_example.py

--- request_example.py
+++ request_example.py
@@ -1,10 +1,6 @@
 import requests
 
-# URL, до якого ви хочете виконати HTTP-запит
-url = "https://www.example.com"  # або будь-який інший URL
-
-# Виконання GET-запиту до вказаного URL
+url = "https://www.example.com"  
 response = requests.get(url)
 
-# Виведення вмісту відповіді на екран
 print(response.text)
